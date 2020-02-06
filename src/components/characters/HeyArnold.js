d2e53240cda4ecac73365f62960b71a9093d4337
TravelFiend <mikegrace274@gmail.com>
2020-02-05 21:19:55 -0800

    paging functions written

--- src/components/characters/HeyArnold.js
+++ src/components/characters/HeyArnold.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Header from '../header/Header';
 import getApiData from '../../services/getApiData';
 import HeyArnoldList from './HeyArnoldList';
+import Paging from '../paging/Paging';
 
 class Final extends Component {
   state = {
     characters: [],
-    oneChar: ''
+    oneChar: '',
+    pageNum: 1
   }
 
   componentDidMount(){
@@ -22,12 +24,27 @@ class Final extends Component {
     this.setState({ oneChar: target.value });
   }
 
+  handleBackClick() {
+    if(this.state.pageNum === 1){
+      return;
+    }
+    this.setState(prevState => ({ pageNum: prevState.pageNum - 1 }));
+  }
+
+  handleForwardClick() {
+    if(this.state.pageNum === 11){
+      return;
+    }
+    this.setState(prevState => ({ pageNum: prevState.pageNum + 1 }));
+  }
+
   render(){
     return (
-      <>
+      <main>
         <Header />
+        <Paging pageNum={this.state.pageNum} onBackClick={this.handleBackClick} onForwardClick={this.handleForwardClick} />
         <HeyArnoldList onClick={this.handleClick} characters={this.state.characters} />
-      </>
+      </main>
     );
   }
 }
---
+++ src/components/paging/Paging.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Paging.css';
+
+const Paging = ({ pageNum, onBackClick, onForwardClick }) => (
+  <div className={styles.Paging}>
+    <button onClick={onBackClick}>&lt;</button>
+    <p>Page: {pageNum}</p>
+    <button onClick={onForwardClick}>&gt;</button>
+  </div>
+);
+
+Paging.propTypes = {
+  pageNum: PropTypes.string.isRequired,
+  onBackClick: PropTypes.func.isRequired,
+  onForwardClick: PropTypes.func.isRequired
+};
+
+export default Paging;
